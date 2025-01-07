8ac31a256a1078d887eeaf6cb6e454796ec6a817
Sahil-Ishan-Tonmoy <sahilishantonmoy@gmail.com>
2025-01-08 01:17:16 +0600

    Delete backend directory

--- backend/models/Employer.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const EmployerSchema = new mongoose.Schema({
-  userId: { type: String, ref: 'User', required: true },
-});
-
-const Employer = mongoose.model('Employer', EmployerSchema);
-
-module.exports = Employer;
